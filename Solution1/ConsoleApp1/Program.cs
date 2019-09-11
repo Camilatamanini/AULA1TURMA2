305c3ca7a43e74e109009b34de2f204c1018be8b
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-09-11 13:59:19 -0300

    Commit

--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -8,11 +8,31 @@ namespace ConsoleApp1
 {
     class Program
     {
+        //iniciando uma constante de um número inteiro
+        const int testeConst = 999;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("teste!");
 
+            string testePrimitivo = string.Empty;
+            String outroTestePrimitivo = string.Empty;
+            bool testeComp = "123" == "123";
+            Boolean testComp = "123" == "123";
+
+
+
+            var testeFora = string.Empty;
+            testeFora = testeFora = "teste";
+
+            var test1 = "String"; //Texto
+            var test2 = 'A'; //Char
+            var test3 = 8; //Int
+            var test4 = 3.8; //Double
+            var test5 = 0x0F; //Hex
+            //Apresentando a Const
+            Console.WriteLine(testeConst);
             Console.ReadKey();
+
 
         }
     }
