64d0d8e95c1c7b9225b341d59cb984c9d75478e6
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-09-11 15:56:53 -0300

    commit

---
+++ MatrizArray/MatrizArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayLinear = new string[3] { "Peralta", "Belinha", "Tito" };
+
+            List<string> listaLinear = new List<string>
+            {
+                "Peralta",
+                "Belinha",
+                "Tito"
+            };
+
+            Console.WriteLine(arrayLinear[0]);
+            Console.WriteLine(listaLinear[0]);
+            var keypress = Console.ReadKey().KeyChar.ToString();
+
+
+        }
+    }
+}
+
+
