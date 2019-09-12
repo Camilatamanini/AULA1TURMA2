c052635325c187d16969e95ea75059dedfd4a046
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-09-12 17:16:36 -0300

    commit

---
+++ RefazendoWhileFor/RefazendoFORdenovo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefazendoFORdenovo
+{
+    class Program
+    {
+        static void Main(string[] args)
+         
+        {
+            Console.WriteLine("Digite a palavra que deseja buscar no texto");
+            var Camila = (Console.ReadLine());
+
+            Console.WriteLine($"Encontados:{Camila}");
+            Console.ReadKey();
+        }
+       
+    }
+}
