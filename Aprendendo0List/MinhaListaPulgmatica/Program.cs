9505d8248c81480a035f50472247a66e1ff1e36a
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-09-16 17:21:39 -0300

    Commit

---
+++ Aprendendo0List/MinhaListaPulgmatica/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaListaPulgmatica
+{
+    class Program
+    {
+        private static List<string> minhaListaPulgmatica01 = new List<string>()
+            {
+                "Camila",
+                "Belinha",
+                "Peralta",
+                "Cristofer"
+            };
+        
+        static void Main(string[] args)
+        {
+            AdicionarItensALista();
+
+            ListaInformacoes();
+           
+           Console.ReadKey();
+        }
+        /// <summary>
+        /// Metodo que adiciona um item na lista
+        /// </summary>
+        private static void AdicionarItensALista()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um nome:");
+            minhaListaPulgmatica01.Add(Console.ReadLine());
+            Console.Clear();
+
+
+            Console.WriteLine("Deseja informar mais valores? sim(S) não (N)");
+
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "s")
+                
+                AdicionarItensALista();
+        }
+
+    /// <summary>
+    /// Metodo que mostra a lista
+    /// </summary>
+        private static void ListaInformacoes()
+        {
+            Console.Clear();
+            Console.WriteLine("Nomes adicionados a lista:");
+            foreach (var item in minhaListaPulgmatica01)
+                Console.WriteLine($"Nome: {item} foi adicionado a lista");
+        }
+
+    }
+
+}
