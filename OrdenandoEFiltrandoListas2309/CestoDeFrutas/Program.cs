94b35f7cb3ac5056be7634b155e20d7cb6236cf9
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-09-23 17:23:43 -0300

    Commit

---
+++ OrdenandoEFiltrandoListas2309/CestoDeFrutas/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CestoDeFrutas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrutasNaCesta();
+        }
+        /// <summary>
+        /// Aqui temos um código inteeeeiro de frutas na cesta em diversas formas
+        /// </summary>
+        static void FrutasNaCesta()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("---------CESTA DE FRUTAS--------");
+            Console.WriteLine(" ");
+
+            var cestaDeFrutas = new List<Fruta>();
+
+            //Tomate
+            cestaDeFrutas.Add(new Fruta()
+            {
+                Id = 0,
+                Nome = "Tomate",
+                Cor = "Vermelho",
+                Peso = 212
+            });
+            //Uva
+            cestaDeFrutas.Add(new Fruta()
+            {
+                Id = 1,
+                Nome = "Uva",
+                Cor = "Roxa",
+                Peso = 4
+            });
+            //Banana
+            cestaDeFrutas.Add(new Fruta()
+            {
+                Id = 2,
+                Nome = "Banana",
+                Cor = "Amarelo",
+                Peso = 86
+            });
+            //Melão
+            cestaDeFrutas.Add(new Fruta()
+            {
+                Id = 3,
+                Nome = "Melão",
+                Cor = "Amarelo",
+                Peso = 1000
+            });
+
+            #region Valores Decrescente Lista1
+
+            //Neste ponto ordenamos os valores de forma decrescente pelo ID utilizando o "OrderByDescending"
+            cestaDeFrutas.
+                OrderByDescending(x => x.Id).ToList<Fruta>().
+                ForEach(i =>
+                Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome} - Peso: {i.Peso}"));
+            #endregion
+
+            #region Valores Crescentes Lista2
+
+            //Neste ponto ordenamos os valores de forma crescente pelo ID utilizando o "OrderBy"
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(" ");
+
+            cestaDeFrutas.
+                OrderBy(x => x.Id).ToList<Fruta>().
+                ForEach(i =>
+                Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome} - Peso: {i.Peso}"));
+
+            #endregion
+
+            #region Filtro de Pesos Lista3
+
+            Console.WriteLine(" ");
+            Console.WriteLine("---------FILTRO PESO-----------");
+            Console.WriteLine(" ");
+
+
+            var filtroCesta = cestaDeFrutas
+                //Aqui filtramos os registros maiores de 100g
+                .Where(x => x.Peso > 100)
+                //Ordenamos estes valores pelos nomes
+                .OrderBy(x => x.Nome);
+            filtroCesta.ToList<Fruta>()
+                .ForEach(i => Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome} - Peso: {i.Peso}"));
+
+            #endregion
+
+            #region Escolhendo a Fruta Lista4
+
+            Console.WriteLine(" ");
+            Console.WriteLine("------ESCOLHENDO A FRUTA-------");
+            Console.WriteLine(" ");
+            (from frutinha in cestaDeFrutas
+             where frutinha.Nome == "Tomate"
+             select frutinha).ToList<Fruta>()
+                              .ForEach(i => Console.WriteLine($"Fruta escolhida {i.Nome}"));
+
+            #endregion
+
+            #region Fruta > 100g Lista5
+
+            Console.WriteLine(" ");
+            Console.WriteLine("---------FRUTA>100G------------");
+            Console.WriteLine(" ");
+            //Frutinha recebe cada fruta de nossa cesta
+            (from frutinha in cestaDeFrutas // Aqui temos a coleção de frutas
+             where frutinha.Peso > 100 // Aqui escolhemos somente a fruta com mais de 100g
+             select frutinha).ToList<Fruta>() //Aqui juntamos a informação filtrada e retorno
+                              .ForEach(i => Console.WriteLine($"Fruta escolhida {i.Nome}")); // 
+            #endregion
+
+            #region Escolhendo a cor Lista6
+
+            Console.WriteLine(" ");
+            Console.WriteLine("-------ESCOLHENDO A COR--------");
+            Console.WriteLine(" ");
+            //Aqui criamos uma variavl que recebera o valor buscado
+            var mostrandoFind = cestaDeFrutas.
+                //Aqui é feito o filtro de informações por uma "ou --> || <---" outra cor
+                //Esse escolhe somente uma cor - esse método retorna somente o primeiro que ele encontrou
+                Find(x => x.Cor == "Amarelo" || x.Cor == "Vermelho");
+
+            Console.WriteLine($"Id: {mostrandoFind.Id} - Nome: {mostrandoFind.Nome}");
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(" ");
+            //Aqui criamos uma variavel que recebera a coleção que estamos buscando
+            //Aqui ele retorna todas as cores que você solicitou
+            var mostrandoFindAll = cestaDeFrutas.
+                //FindAll com esta condição traz frutas de cor amarelha "ou" vermelhas
+                FindAll(X => X.Cor == "Amarelo" || X.Cor == "Vermelho");
+            mostrandoFindAll.ForEach(i => Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome}"));
+
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(" ");
+            //Aqui ordenamos a lista pelo nome 
+            var listaOrdenada = mostrandoFindAll.OrderBy(x => x.Nome);
+            //Como agora não é mais um List<Fruta> usamos o foreach para apresentar os valores 
+            foreach (var item in listaOrdenada)
+                Console.WriteLine($"Id: {item.Id} - Nome: {item.Nome}");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(" ");
+            var cestaDeFrutasFindOrder = cestaDeFrutas
+                //Ordenei minha lista
+                .OrderBy(x => x.Nome)
+                //Converti para lista de frutas 
+                .ToList<Fruta>()
+                //busco minha informação
+                .Find(x => x.Cor == "Amarelo" || x.Cor == "Vermelho");
+            Console.WriteLine($"Id: {cestaDeFrutasFindOrder.Id} - Nome: {cestaDeFrutasFindOrder.Nome}");
+
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OrdenandoEFiltrandoListas2309/RegistroDePessoas/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDePessoas
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------PESSOAS CADASTRADAS---------------------------------");
+            Console.WriteLine(" ");
+
+            var cadastroPessoas = new List<Pessoas>();
+
+            //SPEARS
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 0,
+                Nome = "Spears",
+                DataNascimento = DateTime.Parse("09/07/2011"),
+                Carteira = 846.96
+            });
+            //SWANSON
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 1,
+                Nome = "Swanson",
+                DataNascimento = DateTime.Parse("20/06/2003"),
+                Carteira = 233.09
+            });
+            //GAY
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 2,
+                Nome = "Gay",
+                DataNascimento = DateTime.Parse("03/12/2001"),
+                Carteira = 911.92
+            });
+            //GREGORY
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 3,
+                Nome = "Gregory",
+                DataNascimento = DateTime.Parse("02/01/2000"),
+                Carteira = 469.01
+            });
+            //OSLON
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 4,
+                Nome = "Olson",
+                DataNascimento = DateTime.Parse("18/07/2001"),
+                Carteira = 261.90
+            });
+            //GARZA
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 5,
+                Nome = "Garza",
+                DataNascimento = DateTime.Parse("01/04/2000"),
+                Carteira = 360.41
+            });
+            //SWEET
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 6,
+                Nome = "Sweet",
+                DataNascimento = DateTime.Parse("12/03/2003"),
+                Carteira = 312.76
+            });
+            //CLINE
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 7,
+                Nome = "Cline",
+                DataNascimento = DateTime.Parse("26/03/2002"),
+                Carteira = 484.51
+            });
+            //OLIVER
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 8,
+                Nome = "Oliver",
+                DataNascimento = DateTime.Parse("05/07/2004"),
+                Carteira = 513.76
+            });
+            //VANG
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 9,
+                Nome = "Vang",
+                DataNascimento = DateTime.Parse("10/07/2000"),
+                Carteira = 271.05
+            });
+            //MADDOX
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 10,
+                Nome = "Maddox",
+                DataNascimento = DateTime.Parse("29/05/2004"),
+                Carteira = 783.97
+            });
+            //GARRET
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 11,
+                Nome = "Garret",
+                DataNascimento = DateTime.Parse("03/06/2006"),
+                Carteira = 154.11
+            });
+            //MCINTOSH
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 12,
+                Nome = "Mcintosh",
+                DataNascimento = DateTime.Parse("06/07/2006"),
+                Carteira = 902.80
+            });
+            //YANG
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 13,
+                Nome = "Yang",
+                DataNascimento = DateTime.Parse("29/04/2005"),
+                Carteira = 550.48
+            });
+            //HENDRICKS
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 14,
+                Nome = "Hendricks",
+                DataNascimento = DateTime.Parse("05/09/2003"),
+                Carteira = 410.56
+            });
+            //CAIN
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 15,
+                Nome = "Cain",
+                DataNascimento = DateTime.Parse("12/01/2002"),
+                Carteira = 221.13
+            });
+            //BLACKBURN
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 16,
+                Nome = "Blackburn",
+                DataNascimento = DateTime.Parse("10/05/2000"),
+                Carteira = 135.67
+            });
+            //HOWE
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 17,
+                Nome = "Howe",
+                DataNascimento = DateTime.Parse("27/09/2005"),
+                Carteira = 360.14
+            });
+            //PRATT
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 18,
+                Nome = "Pratt",
+                DataNascimento = DateTime.Parse("18/09/2000"),
+                Carteira = 991.83
+
+            });
+            //SHERMANN
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 19,
+                Nome = "Sherman",
+                DataNascimento = DateTime.Parse("20/02/2003"),
+                Carteira = 667.00
+
+            });
+            //ANA
+            cadastroPessoas.Add(new Pessoas()
+            {
+                Id = 20,
+                Nome = "Anna",
+                DataNascimento = DateTime.Parse("15/09/1998"),
+                Carteira = 900.00
+
+            });
+
+
+
+
+            #region LISTA EM ORDEM CRESCENTE PELO NOME
+
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------ORDEM CRESCENTE PELO NOME------------------------------");
+            Console.WriteLine(" ");
+            //Apresetando a lista em ordem crescente pelo NOME
+            cadastroPessoas.
+                OrderBy(x => x.Nome).ToList<Pessoas>().
+                ForEach(i =>
+                Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome} - Data de nascimento: {i.DataNascimento.ToShortDateString()} - Carteira: {i.Carteira}."));
+            #endregion
+
+            #region LISTA EM ORDEM DECRESCENTE PELA ID 
+            Console.WriteLine(" ");
+            Console.WriteLine("-------------------------------ORDEM DECRESCENTE PELA ID------------------------------");
+            Console.WriteLine(" ");
+            //Apresentando a lista em ordem descrescente pelo ID
+            cadastroPessoas.
+                OrderByDescending(x => x.DataNascimento).ToList<Pessoas>().
+                ForEach(i =>
+                Console.WriteLine($"Id: {i.Id} - Nome: {i.Nome} - Data de nascimento: {i.DataNascimento.ToShortDateString()} - Carteira: {i.Carteira}."));
+            #endregion
+
+            #region LISTA DE PESSOAS COM MAIS DE 500,00 NA CARTEIRA
+            Console.WriteLine(" ");
+            Console.WriteLine("-----------------------------PESSOAS COM MAIS DE 500 REAIS-----------------------------");
+            Console.WriteLine(" ");
+
+            var filtroPessoas = cadastroPessoas
+                //Aqui filtramos as pessoas que possuem mais de 500,00 na carteira
+                .Where(x => x.Carteira > 500)
+                //Ordenamos estes valores em ordem crescente dos NOMES
+                .OrderBy(x => x.Nome);
+            filtroPessoas.ToList<Pessoas>()
+                .ForEach(i => Console.WriteLine($"Nome: {i.Nome} - Carteira: {i.Carteira}."));
+            #endregion
+
+            #region LISTA DE PESSOAS QUE POSSUEM MAIS DE 18 ANOS
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------MAIOR DE IDADE--------------------------------------");
+            Console.WriteLine(" ");
+
+            (from maiorIdade in cadastroPessoas
+             where (DateTime.Today.Year - maiorIdade.DataNascimento.Year) >= 18 
+             select maiorIdade).ToList<Pessoas>()
+                              .ForEach(i => Console.WriteLine($"Nome: {i.Nome} - Data nascimento: {i.DataNascimento.ToShortDateString()}"));
+
+
+            #endregion
+
+            #region LISTA DE PESSOAS QUE POSSUEM MENOS DE 16 ANOS
+            Console.WriteLine(" ");
+            Console.WriteLine("---------------------------------MENOR DE 16 ANOS-------------------------------------");
+            Console.WriteLine(" ");
+
+            (from maiorIdade in cadastroPessoas
+             where (DateTime.Today.Year - maiorIdade.DataNascimento.Year) <= 16 //Diminui a data atual das datas cadastradas
+             select maiorIdade).ToList<Pessoas>()
+                              .ForEach(i => Console.WriteLine($"Nome: {i.Nome} - Data nascimento: {i.DataNascimento.ToShortDateString()}"));
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
