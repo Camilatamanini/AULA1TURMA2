ec04eea85eeaa978e414e464621a3d2303b3ab74
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-10-11 17:00:31 -0300

    jpijp

---
+++ DoBancoAoEntity/InterfaceVendas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceVendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                
+            
+        }
+    }
+}
